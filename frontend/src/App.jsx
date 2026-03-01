729adb5d0aea87a803771d53f8188350efd12a49
Neil Benedict Reyes <neilbcreyes@gmail.com>
2026-03-01 20:58:08 +0800

    React Router and basic pages setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,18 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import LoginPage from './pages/LoginPage'
+import RegisterPage from './pages/RegisterPage'
+import IssuesPage from './pages/IssuesPage'
+
 function App() {
   return (
-    <div>
-      <h1>Linear Clone</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Navigate to='/issues' />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/register' element={<RegisterPage />} />
+        <Route path='/issues' element={<IssuesPage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ frontend/src/pages/IssuesPage.jsx
@@ -0,0 +1,9 @@
+function IssuesPage() {
+  return (
+    <div>
+      <h1>Issues Page</h1>
+    </div>
+  )
+}
+
+export default IssuesPage
